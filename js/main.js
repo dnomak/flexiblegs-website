84a836ba41e85272b82e89289e0e7c92fcba60c2
Doğukan Güven Nomak <me@dnomak.com>
2014-09-09 03:24:39 +0300

    new menu

--- js/main.js
+++ js/main.js
@@ -30,6 +30,81 @@ else {
   scrolltop()
 }
 
+var xlbrowserclick = function() {
+  $('.xlstyle-browser-click').addClass('active');
+  $('.lgstyle-browser-click').removeClass('active');
+  $('.mdstyle-browser-click').removeClass('active');
+  $('.smstyle-browser-click').removeClass('active');
+  $('.xsstyle-browser-click').removeClass('active');
+  $('.browser').removeClass('animated tada');
+  $('.browser').css( "max-width", "1142px" );
+};
+$( ".xlstyle-browser-click" ).click(function() {
+  xlbrowserclick()
+  $("html, body").delay(0).animate({scrollTop: $('.color-circle').offset().top }, 500);
+  $('.browser').addClass('animated tada');
+});
+
+var lgbrowserclick = function() {
+  $('.xlstyle-browser-click').removeClass('active');
+  $('.lgstyle-browser-click').addClass('active');
+  $('.mdstyle-browser-click').removeClass('active');
+  $('.smstyle-browser-click').removeClass('active');
+  $('.xsstyle-browser-click').removeClass('active');
+  $('.browser').removeClass('animated tada');
+  $('.browser').css( "max-width", "1026px" );
+};
+$( ".lgstyle-browser-click" ).click(function() {
+  lgbrowserclick()
+  $("html, body").delay(0).animate({scrollTop: $('.color-circle').offset().top }, 500);
+  $('.browser').addClass('animated tada');
+});
+
+var mdbrowserclick = function() {
+  $('.xlstyle-browser-click').removeClass('active');
+  $('.lgstyle-browser-click').removeClass('active');
+  $('.mdstyle-browser-click').addClass('active');
+  $('.smstyle-browser-click').removeClass('active');
+  $('.xsstyle-browser-click').removeClass('active');
+  $('.browser').removeClass('animated tada');
+  $('.browser').css( "max-width", "770px" );
+};
+$( ".mdstyle-browser-click" ).click(function() {
+  mdbrowserclick()
+  $("html, body").delay(0).animate({scrollTop: $('.color-circle').offset().top }, 500);
+  $('.browser').addClass('animated tada');
+});
+
+var smbrowserclick = function() {
+  $('.xlstyle-browser-click').removeClass('active');
+  $('.lgstyle-browser-click').removeClass('active');
+  $('.mdstyle-browser-click').removeClass('active');
+  $('.smstyle-browser-click').addClass('active');
+  $('.xsstyle-browser-click').removeClass('active');
+  $('.browser').removeClass('animated tada');
+  $('.browser').css( "max-width", "570px" );
+};
+$( ".smstyle-browser-click" ).click(function() {
+  smbrowserclick()
+  $("html, body").delay(0).animate({scrollTop: $('.color-circle').offset().top }, 500);
+  $('.browser').addClass('animated tada');
+});
+
+var xsbrowserclick = function() {
+  $('.xlstyle-browser-click').removeClass('active');
+  $('.lgstyle-browser-click').removeClass('active');
+  $('.mdstyle-browser-click').removeClass('active');
+  $('.smstyle-browser-click').removeClass('active');
+  $('.xsstyle-browser-click').addClass('active');
+  $('.browser').removeClass('animated tada');
+  $('.browser').css( "max-width", "322px" );
+};
+$( ".xsstyle-browser-click" ).click(function() {
+  xsbrowserclick()
+  $("html, body").delay(0).animate({scrollTop: $('.color-circle').offset().top }, 500);
+  $('.browser').addClass('animated tada');
+});
+
 iframe = "<div class='row text-center'><iframe src='"+ url +"' class='example' frameborder='0'></iframe></div>";
 $('.macbook-big').html(iframe);
 
@@ -179,15 +254,198 @@ $( ".smstyle-circle" ).click(function() {
   $("html, body").delay(0).animate({scrollTop: $('.color-circle').offset().top }, 500);
 });
 
-$( ".xsstyle-circle" ).click(function() {
-  xscircle()
-  $("html, body").delay(0).animate({scrollTop: $('.color-circle').offset().top }, 500);
+
+
+
+$( ".navicon" ).click(function() {
+  $("#menu").css("display", "block");
 });
 
-// $( ".navicon" ).click(function() {
-//   $("#menu").css("display", "block");
-// });
+$( ".close" ).click(function() {
+  $("#menu").css("display", "none");
+});
 
-// $( ".close" ).click(function() {
-//   $("#menu").css("display", "none");
-// });
+$(".columns a[href='/24-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>24 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/23-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>23 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/22-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>22 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/21-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>21 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/20-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>20 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/19-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>19 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/18-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>18 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/17-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>17 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/16-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>16 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/15-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>15 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/14-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>14 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/13-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>13 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/12-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>12 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/11-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>11 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/10-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>10 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/9-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>9 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/8-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>8 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/7-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>7 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/6-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>6 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/5-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>5 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/4-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>4 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/3-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>3 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
+$(".columns a[href='/2-column-grid']").hover(
+  function(e){
+    $(".hover-change").html("<b>2 Column</b> Grid")
+  },
+  function(e){
+    $(".hover-change").html("<b>All Columns</b>")
+  }
+);
